6528682e7bf7e38abc3935248969ee96bbd98937
morgankar <realrohankar03@gmail.com>
2025-05-03 20:54:55 +0530

    created state and component like framework

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
 let ctr = 1
+// function addTodo(){
+//     const inpText = document.getElementById("input").value
+//     const newDiv = document.createElement("div");
+//     const newSpan= document.createElement("span")
+//     const newBtn = document.createElement("button")
+
+//     //Add attributes
+//     newBtn.setAttribute("onclick",`deleteTodo(${ctr})`)
+//     newDiv.setAttribute("id","todo-"+ctr)
+//     newSpan.setAttribute("id","todoEntryText-" +ctr)
+
+//     document.querySelector("body").appendChild(newDiv);
+//     //Append span and btn to newDiv
+//     newSpan.innerText = `${ctr}. ${inpText}`
+//     newBtn.innerText = "Delete"
+//     newDiv.appendChild(newSpan)
+//     newDiv.appendChild(newBtn)
+//     ctr++;
+// }
+
+// function deleteTodo(ctr){
+//     const element = document.getElementById(`todo-${ctr}`)
+//     element.parentNode.removeChild(element)
+// }
+
+let todos =[];
 function addTodo(){
-    const inpText = document.getElementById("input").value
-    const newDiv = document.createElement("div");
-    const newSpan= document.createElement("span")
-    const newBtn = document.createElement("button")
-
-    //Add attributes
-    newBtn.setAttribute("onclick",`deleteTodo(${ctr})`)
-    newDiv.setAttribute("id","todo-"+ctr)
-    newSpan.setAttribute("id","todoEntryText-" +ctr)
-
-    document.querySelector("body").appendChild(newDiv);
-    //Append span and btn to newDiv
-    newSpan.innerText = `${ctr}. ${inpText}`
-    newBtn.innerText = "Delete"
-    newDiv.appendChild(newSpan)
-    newDiv.appendChild(newBtn)
-    ctr++;
+    todos.push({
+        title: document.querySelector("input").value
+    })
+    render()
 }
 
-function deleteTodo(ctr){
-    const element = document.getElementById(`todo-${ctr}`)
-    element.parentNode.removeChild(element)
+function deleteTodo(){
+    render()
+}
+
+function render(){
+    document.getElementById("todoCard").innerHTML ="";
+    for(let i=0;i <todos.length ;i++){
+        const newSpan= document.createElement("span")
+        const newBtn = document.createElement("button")
+        const newDiv = document.createElement("div");
+        newSpan.innerText = todos[i].title
+        newBtn.innerText = "Delete"
+        document.getElementById("todoCard").appendChild(newDiv);
+        newDiv.appendChild(newSpan)
+        newDiv.appendChild(newBtn)
+        
+    }
+    
+  
+    
 }
